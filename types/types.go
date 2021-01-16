d986f7b993c0e32b2905b98f5a51f7f210c58c55
dmitry <yashulyak@gmail.com>
2021-01-16 10:16:56 +0200

    Generate protobuf types for raft

--- types/types.go
+++ types/types.go
@@ -1,12 +1,9 @@
 package types
 
 import (
-	"context"
 	"fmt"
 	"net"
 	"strconv"
-
-	"github.com/dshulyak/raft/raftlog"
 )
 
 type NodeID uint64
@@ -21,43 +18,6 @@ func NodeIDFromString(id string) (NodeID, error) {
 		return 0, err
 	}
 	return NodeID(nid), nil
-}
-
-type LogHeader struct {
-	Term, Index uint64
-}
-
-type Message interface{}
-
-type RequestVote struct {
-	Term      uint64
-	Candidate NodeID
-	PreVote   bool
-	LastLog   LogHeader
-}
-
-type RequestVoteResponse struct {
-	Term        uint64
-	Voter       NodeID
-	PreVote     bool
-	VoteGranted bool
-}
-
-type AppendEntries struct {
-	Term      uint64
-	Leader    NodeID
-	PrevLog   LogHeader
-	Commited  uint64
-	ReadIndex uint64
-	Entries   []*raftlog.LogEntry
-}
-
-type AppendEntriesResponse struct {
-	Term      uint64
-	Follower  NodeID
-	Success   bool
-	LastLog   LogHeader
-	ReadIndex uint64
 }
 
 type Node struct {
@@ -90,81 +50,4 @@ var confChangeString = [...]string{"Empty", "Add", "Delete"}
 type ConfChange struct {
 	Type ConfChangeType
 	Node Node
-}
-
-func NewReadRequest(ctx context.Context) *Proposal {
-	return &Proposal{
-		ctx:  ctx,
-		errc: make(chan error, 1),
-		read: true,
-	}
-}
-
-func NewProposal(ctx context.Context, entry *raftlog.LogEntry) *Proposal {
-	return &Proposal{
-		ctx:     ctx,
-		errc:    make(chan error, 1),
-		resultc: make(chan interface{}, 1),
-		Entry:   entry,
-	}
-}
-
-type Proposal struct {
-	ctx     context.Context
-	errc    chan error
-	read    bool
-	resultc chan interface{}
-	Entry   *raftlog.LogEntry
-}
-
-func (p *Proposal) Read() bool {
-	return p.read
-}
-
-func (p *Proposal) WaitResult(ctx context.Context) (interface{}, error) {
-	if p.ctx == nil {
-		panic("proposal is noop")
-	}
-	select {
-	case <-ctx.Done():
-		return nil, ctx.Err()
-	case result := <-p.resultc:
-		return result, nil
-	case <-p.ctx.Done():
-		return nil, p.ctx.Err()
-	}
-}
-
-func (p *Proposal) Apply(result interface{}) {
-	if p.ctx == nil {
-		return
-	}
-	select {
-	case <-p.ctx.Done():
-	case p.resultc <- result:
-	}
-}
-
-func (p *Proposal) Complete(err error) {
-	if p.ctx == nil {
-		return
-	}
-	select {
-	case <-p.ctx.Done():
-	case p.errc <- err:
-	}
-}
-
-func (p *Proposal) Wait(ctx context.Context) error {
-	if p.ctx == nil {
-		panic("proposal is noop")
-	}
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	case err := <-p.errc:
-		return err
-	case <-p.ctx.Done():
-		return p.ctx.Err()
-	}
 }
